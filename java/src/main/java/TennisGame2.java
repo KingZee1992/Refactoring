90b6232770fcbf501399b9857e923492bd1da00d
KingZee1992 <90901601+KingZee1992@users.noreply.github.com>
2021-09-17 13:30:33 +0300

    Update TennisGame2.java

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,7 +1,7 @@
 
 public class TennisGame2 implements TennisGame
 {
-    //testing commit and push...
+    //First major commit and push...
     public int P1point = 0;
     public int P2point = 0;
     
@@ -17,67 +17,33 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
+        String[] scoreTitle = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+
         if (P1point == P2point && P1point < 4)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+            score = scoreTitle[P1point] + "-All";
         }
         if (P1point==P2point && P1point>=3)
             score = "Deuce";
         
         if (P1point > 0 && P2point==0)
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
             P2res = "Love";
-            score = P1res + "-" + P2res;
+            score = scoreTitle[P1point] + "-" + P2res;
         }
         if (P2point > 0 && P1point==0)
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
             P1res = "Love";
-            score = P1res + "-" + P2res;
+            score = P1res + "-" + scoreTitle[P2point];
         }
         
         if (P1point>P2point && P1point < 4)
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+            score = scoreTitle[P1point] + "-" + scoreTitle[P2point];
         }
         if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+            score = scoreTitle[P1point] + "-" + scoreTitle[P2point];
         }
         
         if (P1point > P2point && P2point >= 3)
